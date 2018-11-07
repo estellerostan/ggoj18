6cce5473a3fbadec1a732d3b78d312873e377f5e
Nomanoclass <bart.simpson124@gmail.com>
2018-11-07 22:04:04 +0100

    Tests avec tuto https://www.youtube.com/watch?v=6jUWEKFk9oI https://www.youtube.com/watch?v=DS2BA79qsrQ

---
+++ Scripts/Spatial.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Spatial : Godot.Spatial
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    RigidBody player_1;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        player_1 = (RigidBody)GetNode("Player_1");
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+}
